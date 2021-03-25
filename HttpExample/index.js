b892d6b9cc0e86d79460cac15c53340fd93a8fea
Anh Vu <vu.haianh291@gmail.com>
2021-03-25 11:46:06 +0200

    Feat: Connect to cosmosdb

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,13 +1,40 @@
+const mongoClient = require('mongodb').MongoClient
+let db
+mongoClient.connect(
+  'mongodb://functionstests2403:vadVJkuHQD5n141PnvBpqXGEZHmyBytmg4X3kYiJSodWZIRinRtFuwrigSAT5kQyg0U5vErCI6pHfEh67LDmqg%3D%3D@functionstests2403.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@functionstests2403@',
+  (err, client) => {
+    db = client.db('functionstests2403')
+  }
+)
+let studentList = [
+  {
+    name: 'Anh Vu',
+    email: 'anh.vu@stu.com',
+  },
+  {
+    name: 'Hai Vu',
+    email: 'hai.vu@stu.com',
+  },
+  {
+    name: 'John Smith',
+    email: 'john.smith@stu.com',
+  },
+]
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+  if (req.method === 'GET') {
+    const collection = db.collection('students')
+    const list = await collection.find({}).toArray()
+    console.log(list)
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+      // status: 200, /* Defaults to 200 */
+      body: JSON.stringify(list),
+    }
+  } else {
+    const collection = db.collection('students')
+    await collection.insertMany(studentList)
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: 'Inserted',
+    }
+  }
 }
